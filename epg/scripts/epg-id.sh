00dda19225876ce79c88c6319642bebcd0da5dfa
didikc <166754124+didikc@users.noreply.github.com>
2026-08-08 17:31:12 +0700

    Update epg-id.sh

--- epg/scripts/epg-id.sh
+++ epg/scripts/epg-id.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Go into iptv-org-epg repo and install dependencies
-cd /home/runner/work/EPG-2/EPG-2/iptv-org-epg
+cd iptv-org-epg
 npm install
 
 # Generate Pix EPG
@@ -12,7 +12,7 @@ npm run grab --- --channels=../epg/scripts/pix.xml --output=../epg/pix.xml --day
 cd ../epg
 gzip -k -f -9 pix.xml
 
-# Optional cleanup (currently disabled)
+# Optional cleanup
 # rm id*.xml.gz
 # rm pix.xml
 
